961ffb6179e74a167713b744cd9a51e89c22ac84
marco.at <marco.at@outlook.pt>
2023-01-23 11:08:15 +0100

    Create pipeline for paper

--- annotate.py
+++ annotate.py
@@ -4,13 +4,54 @@ Outputs the annotations to an Annotations folder. Requires a FASTA file
 with all products concatenated.
 '''
 from annotations.prokka import batch_annotate
+from annotations.databases import cat_genbank
 import yaml
-import sys
-import getopt
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(
+    description = 'Annotate plasmid FASTA nucleotide sequences in the path specified in data_config/paths/plasmids, using Prokka.'
+)
+parser.add_argument(
+    '--build', 
+    action = 'store_const',
+    const = True, 
+    default = False, 
+    help='Builds a protein FASTA database with all plasmid features. Must be set at least once.'
+)
+args = parser.parse_args()
 
 if __name__ == "__main__":
     with open('./configs/data_config.yaml', 'r') as config_file:
         config = yaml.load(config_file, Loader=yaml.Loader)
+
+    if args.build:
+        cat_genbank(
+            config['paths']['plasmids_gb'],
+            os.path.join(
+                config['paths']['plasmids_gb'],
+                'concat_features.gb'
+            ),
+            format='gb',
+            bash = False
+        )
+        process = subprocess.run(
+            [
+                'makeblastdb',
+                '-dbtype',
+                'prot',
+                '-in',
+                os.path.join(
+                    config['paths']['plasmids_gb'],
+                    'concat_features.gb'
+                ),
+                '-out',
+                config['paths']['proteins']
+            ]
+        )
+        assert process.returncode != 0, 'Failed to execute makeblastdb.'
+
 
     batch_annotate(
         config['paths']['plasmids'],
--- ismb_pipeline.sh
+++ ismb_pipeline.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# Create concatenated GBFF file for all plasmid features
+# Annotate plasmids
+python annotate.py --build
+
