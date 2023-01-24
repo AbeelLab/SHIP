7b9c43a23878669d1ae1102ce45d35c3ba46ae9d
marco.at <marco.at@outlook.pt>
2023-01-24 11:12:30 +0100

    Add find_amr.py

---
+++ find_amr.py
@@ -0,0 +1,33 @@
+
+'''
+Calls AMRFinder+ to find AMR genes in all plasmids in 
+data_config/paths/amrfinder_output
+'''
+
+import subprocess
+import os
+import yaml
+
+if __name__ == "__main__":
+    with open('configs/data_config.yaml', 'r') as config_file:
+        config = yaml.load(config_file, Loader=yaml.Loader)
+
+    files = []
+    for x in list(
+        os.walk(config['paths']['plasmids'])
+    )[1:]:
+        for xx in x[-1]:
+            if xx.endswith('.fa') or xx.endswith('.fasta') or xx.endswith('.faa'):
+                subprocess.run(
+                    [
+                        'amrfinder',
+                        '-n',
+                        os.path.join(x[0], xx),
+                        '-o',
+                        os.path.join(
+                            config['paths']['amrfinder_output'],
+                            xx.split('.')[0]+'.txt'
+                        )
+                    ]
+                )
+    
