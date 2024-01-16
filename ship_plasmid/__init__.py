695a0f42b649d1de849ab85bd5707bc01f292f96
Marco Teixeira <mcarvalh@broadinstitute.org>
2024-01-16 11:04:41 -0500

    Changes that whent nowere

--- ship_plasmid/__init__.py
+++ ship_plasmid/__init__.py
@@ -1,5 +1,5 @@
 """Finds regions with evidence of HGT between plasmids and groups plasmids according to evolutionary dynamics."""
 
-__version__="0.2.3"
+__version__="0.2.4"
 
 from ship_plasmid.ship import main
